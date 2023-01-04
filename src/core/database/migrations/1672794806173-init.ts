6c02301a9d01a7c3d145d9dae62fff36c50a5905
Anderson Aguiar <andersonlaguiar@gmail.com>
2023-01-03 22:27:25 -0300

    feat: add migrations settings for database versioning

---
+++ src/core/database/migrations/1672794806173-init.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class init1672794806173 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
